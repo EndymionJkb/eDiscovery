24ddedf1e316ba500f75bd4f8f32f477f4130bb4
Bruce McElroy <bruce.mcelroy@gmail.com>
2014-09-25 05:59:11 -0400

    added active link helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,12 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+  # add active link to nav
+  def nav_link(link_text, link_path)
+  class_name = current_page?(link_path) ? 'active' : ''
+
+  content_tag(:li, :class => class_name) do
+    link_to link_text, link_path
+  end
+end
 end
